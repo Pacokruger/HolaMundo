856850902e0d802a8e41a3ca211ba384dd1d9713
“Pacokruger” <paco10_09@hotmail.com>
2023-05-15 21:52:18 -0300

    prueba git

--- JavaApplication42/src/javaapplication42/JavaApplication42.java
+++ JavaApplication42/src/javaapplication42/JavaApplication42.java
@@ -16,6 +16,11 @@ public class JavaApplication42 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        git status // ver lo nuevo
+                git add .  // para agregar los archivos nuevos
+                git commit -m " nombre" // para darle un nombre a las cosas que subis
+                git push  // para subir todo a github
     }
     
 }
